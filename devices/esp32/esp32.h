06c221fdb4c62113a82f087293516a25e5aa16ef
suraji <surajinamdar151@gmail.com>
2025-08-26 01:17:58 +0530

    Updating tcp mutex wrapper for device esp32 tcp_* calls

--- devices/esp32/esp32.h
+++ devices/esp32/esp32.h
@@ -36,6 +36,8 @@ created Date    : 1st Jan 2024
 
 #include <lwip/tcp.h>
 #include <lwip/inet.h> // For IP address conversion
+#include <lwip/opt.h>
+#include <lwip/tcpip.h>
 
 // extern "C"
 // {
@@ -126,6 +128,52 @@ created Date    : 1st Jan 2024
 //   #define ENABLE_NAPT_FEATURE_LWIP_V2
 // #endif
 
+#ifdef LOCK_TCPIP_CORE
+#define LOCK_LWIP_TCPIP_CORE LOCK_TCPIP_CORE()
+#else
+#define LOCK_LWIP_TCPIP_CORE ((void)0)
+#endif
+
+#ifdef UNLOCK_TCPIP_CORE
+#define UNLOCK_LWIP_TCPIP_CORE UNLOCK_TCPIP_CORE()
+#else
+#define UNLOCK_LWIP_TCPIP_CORE ((void)0)
+#endif
+
+// Value-returning wrapper
+#define TCP_LOCKED_CALL_RETVAL(expr)                               \
+    ({                                                             \
+        bool _need_lock = !sys_thread_tcpip(LWIP_CORE_LOCK_QUERY_HOLDER); \
+        if (_need_lock) { LOCK_LWIP_TCPIP_CORE; }                  \
+        auto _res = (expr);                                        \
+        if (_need_lock) { UNLOCK_LWIP_TCPIP_CORE; }                \
+        _res;                                                      \
+    })
+
+// Void-returning wrapper
+#define TCP_LOCKED_CALL_RETVOID(expr)                              \
+    do {                                                           \
+        bool _need_lock = !sys_thread_tcpip(LWIP_CORE_LOCK_QUERY_HOLDER); \
+        if (_need_lock) { LOCK_LWIP_TCPIP_CORE; }                  \
+        (expr);                                                    \
+        if (_need_lock) { UNLOCK_LWIP_TCPIP_CORE; }                \
+    } while (0)
+
+// Now redefine the functions you use
+#define tcp_new()                 TCP_LOCKED_CALL_RETVAL(tcp_new())
+#define tcp_bind(pcb, ip, port)   TCP_LOCKED_CALL_RETVAL(tcp_bind(pcb, ip, port))
+#define tcp_listen(pcb)           TCP_LOCKED_CALL_RETVAL(tcp_listen_with_backlog(pcb, TCP_DEFAULT_LISTEN_BACKLOG))
+#define tcp_accept(pcb, accept)   TCP_LOCKED_CALL_RETVOID(tcp_accept(pcb, accept))
+#define tcp_arg(pcb, arg)         TCP_LOCKED_CALL_RETVOID(tcp_arg(pcb, arg))
+#define tcp_sent(pcb, arg)         TCP_LOCKED_CALL_RETVOID(tcp_sent(pcb, arg))
+#define tcp_recv(pcb, arg)         TCP_LOCKED_CALL_RETVOID(tcp_recv(pcb, arg))
+#define tcp_err(pcb, arg)         TCP_LOCKED_CALL_RETVOID(tcp_err(pcb, arg))
+#define tcp_write(pcb, data, len, flags) TCP_LOCKED_CALL_RETVAL(tcp_write(pcb, data, len, flags))
+#define tcp_output(pcb)           TCP_LOCKED_CALL_RETVAL(tcp_output(pcb))
+#define tcp_recved(pcb, len)      TCP_LOCKED_CALL_RETVOID(tcp_recved(pcb, len))
+#define tcp_close(pcb)            TCP_LOCKED_CALL_RETVAL(tcp_close(pcb))
+#define tcp_abort(pcb)            TCP_LOCKED_CALL_RETVOID(tcp_abort(pcb))
+#define tcp_connect(pcb, ip, port, connected) TCP_LOCKED_CALL_RETVAL(tcp_connect(pcb, ip, port, connected))
 
 #define NAPT_INIT_DURATION_AFTER_WIFI_CONNECT MILLISECOND_DURATION_5000
 
